52376ff259475d52bd8c3fa96e67833fe25c8642
Andrew Friesen <99490619+Wrekky@users.noreply.github.com>
2025-08-11 23:21:12 -0400

    added deltatime

--- src/Application.cpp
+++ src/Application.cpp
@@ -41,10 +41,10 @@ void Application::Update() {
     // TODO: update all objects in the scene
     //Check fps
     static int timePreviousFrame;
-
+    float deltaTime = (SDL_GetTicks() - timePreviousFrame) / 1000.0f;
     int physicsTime = MILLISECS_PER_FRAME - (SDL_GetTicks() - timePreviousFrame);
     //All physics code, TODO: split into a seperate function/class
-    particle->velocity = Vec2(2.0,2.0);
+    particle->velocity = Vec2(50.0 * deltaTime, 50.0  * deltaTime);
     particle->position += (particle->velocity);
     /////////////////////////////////////////////
     int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - timePreviousFrame);;
