a17e56cdedc678e5ef005943e432b21e2f517d5d
Andrew Friesen <99490619+Wrekky@users.noreply.github.com>
2025-10-28 19:51:42 -0400

    Added scene files & todos.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,19 @@
 #include "Application.h"
 
 int main(int argc, char *args[]) {
+    //TODO: Multiple scenes
+    //If scene 1:
     Application app;
-
     app.Setup();
-
     while (app.IsRunning()) {
         app.Input();
         app.Update();
         app.Render();
     }
-
     app.Destroy();
+    //If scene 2:
+
+    
 
     return 0;
 }
---
+++ src/Scene/Scene.cpp
@@ -0,0 +1,4 @@
+//TODO: Make scene class, should inherit from application class but only override game object setup & player input. Current application.cpp will be a scene. Render should be universal.
+//Will have multiple scene files defined like Scene1, Scene2, or just describing what they are.
+//Will be similar to the shape class but split into multiple files due to expected size of each scene being pretty large. 
+//Will have a method of switching scenes while playing, don't know the method yet.
