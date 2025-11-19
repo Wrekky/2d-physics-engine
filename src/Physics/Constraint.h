59553d3a7a7714ee1bdca93c301d4507f24e4f17
Andrew Friesen <99490619+Wrekky@users.noreply.github.com>
2025-11-18 21:57:14 -0500

    filled out skeleton of the constraint class.

--- src/Physics/Constraint.h
+++ src/Physics/Constraint.h
@@ -2,16 +2,26 @@
 #define CONSTRAINT_H
 
 #include "Body.h"
-
+#include "MatMN.h"
 class Constraint {
     public:
-        //Body* a;
-        //Body* b;
+        Body* a;
+        Body* b;
+
+        virtual ~Constraint() = default;
 
         //TODO Helper matrix & vector class:
-        //MatMN GetInvM(6, 6);
-        //VecN vec();
-        //void Solve();
+        MatMN GetInvM() const;
+        VecN GetVelocities() const;
+
+        virtual void Solve() {}
 };
 
+class DistanceConstraint : public Constraint {
+    //void Solve() override;
+};
+
+class PenetrationConstraint : public Constraint {
+    //void Solve() override;
+};
 #endif
